e4ca45aacf3613d408d49dea82cbd94f7c0789f1
Elliott <ellbutt@gmail.com>
2024-06-19 12:32:45 -0230

    moved around files

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main { 
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Library library = new Library();
